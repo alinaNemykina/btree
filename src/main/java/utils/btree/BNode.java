7581bbb1720980255705fd21da8c016532cf769c
Немыкина Алина Николаевна <aalnemykina11400@gmail.com>
2020-12-05 05:56:32 +0300

    Testing constructor Node

---
+++ src/main/java/utils/btree/BNode.java
@@ -0,0 +1,21 @@
+package utils.btree;
+
+public class BNode {
+    public int maxSize;
+    public int count;
+    public int[] value;
+    public int[] key;
+    public BNode[] child;
+    public boolean leaf;
+    public BNode parent;
+
+    public BNode(int maxSize, BNode parent) {
+        this.maxSize = maxSize;
+        this.parent = parent;
+        this.value = new int[2 * maxSize - 1];
+        this.key = new int[2 * maxSize - 1];
+        this.child = new BNode[2 * maxSize];
+        this.leaf = true;
+        this.count = 0;
+    }
+}
---
+++ src/test/java/test/BTreeTest.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.junit.Test;
+import utils.btree.BNode;
+import utils.btree.BTree;
+import static org.junit.Assert.*;
+
+public class BTreeTest {
+
+    @Test
+    public void createBNode() {
+        BNode bNode = new BNode(3, null);
+        assertEquals(bNode.maxSize, 3);
+        assertNull(bNode.parent);
+        assertNotNull(bNode.value);
+        assertNotNull(bNode.key);
+        assertNotNull(bNode.child);
+        assertTrue(bNode.leaf);
+        assertEquals(bNode.count, 0);
+    }
+}
